45ee9e31d9273019141213e0fde99385a8b0f455
Serena Stater <srstater@gmail.com>
2018-05-08 21:00:35 -0400

    building dataframe

--- bikeshare.py
+++ bikeshare.py
@@ -6,6 +6,16 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+city = []
+month = []
+day = []
+
+def error_mess():
+    """
+    Prints error statements for exceptions
+    """
+    print("Sorry, I didn't understand that.")
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -22,43 +32,43 @@ def get_filters():
     # HINT: Use a while loop to handle invalid inputs
     while True:
         try:
-            city = str(input("You may see stats from either Chicago, New York \
-            City, or Washington. Please enter your chosen city! ").lower())
+            city = str(input("You may see stats from either Chicago, New York City, or Washington. Please enter your chosen city! ").lower())
+        except KeyboardInterrupt:
+            print("Bye!")
         except:
-            print("Sorry, I didn't understand that.")
+            error_mess()
             continue
 
         if city.lower() not in ('chicago', 'new york city', 'washington'):
-            print("Sorry, I didn't understand that.")
+            error_mess()
         else:
             break
         # TO DO: get user input for month (all, january, february, ... , june)
     while True:
         try:
-            month = str(input("For which month would you like to see {}'s data? \
-            If you would like to see an entire calendar year, please specify \
-            'all'. ".format(city.title())))
+            month = str(input("For which month would you like to see {}'s data? If you would like to see an entire calendar year, please specify 'all months'. ".format(city.title())))
+        except KeyboardInterrupt:
+            print("Bye!")
         except:
-            print("Sorry, I didn't understand that.")
+            error_mess()
             continue
 
-        if month.lower() not in ('january', 'february', 'march', 'april', 'may',
-        'june', 'july', 'august', 'september', 'october', 'november', 'december'):
-            print("Sorry, I didn't understand that.")
+        if month.lower() not in ('january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december', 'all months'):
+            error_mess()
         else:
             break
         # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
     while True:
         try:
-            day = int(input("For which day of {} would you like to see data? \
-            If you would like to see the entire month, please specify \
-            'all'. ".format(month.title())))
+            day = int(input("For which day of {} would you like to see data? If you would like to see every day, please specify '0'. ".format(month.title())))
+        except KeyboardInterrupt:
+            print("Bye!")
         except:
-            print("Sorry, I didn't understand that.")
+            error_mess()
             continue
 
         if day not in range(31):
-            print("Sorry, I didn't understand that.")
+            error_mess()
         else:
             break
 
@@ -66,23 +76,32 @@ def get_filters():
     return city, month, day
 
 get_filters()
-#
-# def load_data(city, month, day):
-#     """
-#     Loads data for the specified city and filters by month and day if applicable.
-#
-#     Args:
-#         (str) city - name of the city to analyze
-#         (str) month - name of the month to filter by, or "all" to apply no month filter
-#         (str) day - name of the day of week to filter by, or "all" to apply no day filter
-#     Returns:
-#         df - Pandas DataFrame containing city data filtered by month and day
-#     """
-#
-#
-#     return df
-#
-#
+
+def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
+    if city == 'chicago':
+        city_df = pd.read_csv('chicago.csv')
+    elif city == 'new york city':
+        city_df = pd.read_csv('new_york_city.csv')
+    else:
+        # city_df = pd.read_csv('washington.csv')
+        print("else is running")
+
+    print(city_df.head())
+
+    return city_df
+
+load_data(city, month, day)
+
 # def time_stats(df):
 #     """Displays statistics on the most frequent times of travel."""
 #
